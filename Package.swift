be695af03bbd7351eb154e691407faf3fc47ae9a
Dane Walton <danerwalton@outlook.com>
2021-11-12 15:10:30 -0800

    update package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AzureSDKforSwift",
-            dependencies: []),
+            dependencies: [
+                .product(name: "MQTT", package: "swift-mqtt")]
+        ),
         .testTarget(
             name: "AzureSDKforSwiftTests",
             dependencies: ["AzureSDKforSwift"]),
